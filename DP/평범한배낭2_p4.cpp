e09804c0d964e8a37efba0078dd7e7da01a4c313
jongwook98 <oucsa1541@naver.com>
2022-11-28 17:23:40 +0900

    [Jongwook] Create DP
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ DP/평범한배낭2_p4.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int DP[10001][10001];
+int V[101];
+int W[101];
+vector<pair<int, int>> v;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M, k;
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> W[i] >> V[i] >> k;
+		for (int j = 1; k > 0; j <<= 1) {
+			int ix = min(j, k);
+			v.push_back({ W[i] * ix, V[i] * ix });
+			k -= ix;
+		}
+	}
+
+	int cnt = 1;
+	for (pair<int, int> info : v) {
+		for (int j = 1; j <= M; j++) {
+			if (j - info.first >= 0) DP[cnt][j] = max(DP[cnt - 1][j], DP[cnt - 1][j - info.first] + info.second);
+			else DP[cnt][j] = DP[cnt - 1][j];
+		}
+		cnt++;
+	}
+
+	/*for (int i = 1; i <= 3; i++) {
+		for (int j = 1; j <= 9; j++) {
+			cout << DP[i][j] << " ";
+		}
+		cout << '\n';
+	}*/
+
+	cout << DP[v.size()][M] << '\n';
+
+	return 0;
+}
---
+++ DP/평범한배낭_g5.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int DP[101][100001];
+int V[101];
+int W[101];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, K;
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> W[i] >> V[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			if (j - W[i] >= 0) DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + V[i]);
+			else DP[i][j] = DP[i - 1][j];
+		}
+	}
+	cout << DP[N][K] << '\n';
+
+	return 0;
+}
