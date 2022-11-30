20945d701f8955b4e8462da0b503cb10d4319c41
jongwook98 <oucsa1541@naver.com>
2022-11-30 20:55:59 +0900

    [Jongwook] Add 구현문제
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ 구현/로봇청소기.cpp
@@ -0,0 +1,70 @@
+﻿#include<iostream>
+
+using namespace std;
+
+typedef struct {
+	int y, x, dir;
+}info;
+
+bool map[50][50];
+bool check[50][50];
+
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, 1, 0, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M, r, c, d;
+	cin >> N >> M >> r >> c >> d;
+
+	int cy = r;
+	int cx = c;
+	int cd = d;
+
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < M; x++) {
+			cin >> map[y][x];
+		}
+	}
+
+	while (true) {
+		bool fig = true;
+		check[cy][cx] = true;
+		for (int i = 0; i < 4; i++) {
+			int ny = cy + dy[(cd + 3) % 4];
+			int nx = cx + dx[(cd + 3) % 4];
+			if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
+				if (!check[ny][nx] && !map[ny][nx]) {
+					cy = ny; cx = nx;
+					fig = false;
+					cd = (cd + 3) % 4;
+					break;
+				}
+			}
+			cd = (cd + 3) % 4;
+		}
+		if (fig) {
+			int ny = cy + dy[(cd + 2) % 4];
+			int nx = cx + dx[(cd + 2) % 4];
+			if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
+				if (!map[ny][nx]) {
+					cy = ny; cx = nx;
+				}
+				else break;
+			}
+			else break;
+		}
+	}
+	int ans = 0;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < M; x++) {
+			if (check[y][x] == true) ans++;
+		}
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
---
+++ 구현/컨테이어벨트위의로봇.cpp
@@ -0,0 +1,68 @@
+﻿#include<iostream>
+#include<deque>
+
+using namespace std;
+
+typedef struct {
+	int de;
+	bool r;
+}pl;
+
+pl deg[200];
+
+deque<int> dq;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, K;
+	cin >> N >> K;
+
+	int sp = 0;
+	int ep = N - 1;
+
+	int z_p = 0;
+
+	for (int i = 0; i < 2 * N; i++) {
+		cin >> deg[i].de;
+		if (deg[i].de == 0) z_p += 1;
+	}
+
+	int time = 0;
+	while (z_p < K) {
+		sp = (sp + 2 * N - 1) % (2 * N);
+		ep = (ep + 2 * N - 1) % (2 * N);
+		int rep = dq.size();
+		while (rep--) {
+			int cur_r = dq.front();
+			dq.pop_front();
+			deg[cur_r].r = false;
+			if (cur_r == ep) continue;
+			int n_r = (cur_r + 1) % (2 * N);
+			if (deg[n_r].de > 0 && !deg[n_r].r) {
+				deg[n_r].r = true;
+				deg[n_r].de -= 1;
+				if (deg[n_r].de == 0) z_p++;
+				if (n_r == ep) continue;
+				dq.push_back(n_r);
+			}
+			else {
+				dq.push_back(cur_r);
+				deg[cur_r].r = true;
+			}
+		}
+		if (deg[sp].de > 0) {
+			dq.push_back(sp);
+			deg[sp].r = true;
+			deg[sp].de -= 1;
+			if (deg[sp].de == 0) z_p++;
+		}
+		time++;
+	}
+
+	cout << time << '\n';
+
+	return 0;
+}
