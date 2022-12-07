815c37e4cb382b81506f9b7e24fd59fbf86e62fd
jongwook98 <oucsa1541@naver.com>
2022-12-07 18:46:20 +0900

    [Jongwook] Add 문제해결
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ softeer/level3/통근버스출발순서검증하기.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int arr[5000];
+int cnt[5000][5000];
+long long ans[5000][5000];
+
+int main(int argc, char** argv)
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) cin >> arr[i];
+
+	for (int i = 0; i < N - 2; i++) {
+		for (int k = i + 2; k < N; k++) {
+			if (arr[i] > arr[k]) cnt[i][k] = 1;
+		}
+	}
+
+	for (int i = 0; i < N - 2; i++) {
+		int sum_ = 0;
+		for (int k = i + 2; k < N; k++) {
+			ans[i][k] = cnt[i][k] + sum_;
+			sum_ += cnt[i][k];
+		}
+	}
+
+	// 자료형 주의하기....!
+	long long res = 0;
+	for (int i = 0; i < N - 2; i++) {
+		for (int j = i + 1; j < N - 1; j++) {
+			if (arr[i] < arr[j]) res += ans[i][N - 1] - ans[i][j];
+		}
+	}
+
+	cout << res << '\n';
+
+	return 0;
+}
---
+++ 구현/마법사상어와비바라기.cpp
@@ -0,0 +1,105 @@
+﻿#include<iostream>
+#include<queue>
+#include<cstring>
+
+using namespace std;
+
+typedef struct {
+	int y, x;
+}point;
+
+typedef struct {
+	int d, s;
+}info;
+
+int Map[50][50];
+bool check[50][50];
+
+point mv[] = { {0, -1}, {-1, -1}, {-1, 0}, { -1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1 } };
+point dia[] = { {-1, -1 }, {-1, 1}, {1, -1}, {1, 1} };
+queue<info> q;
+queue<point> q_cl;
+queue<point> q_cl_m;
+
+void cloud_magic(int N, int d, int s) {
+
+	memset(check, false, sizeof(check));
+	
+	// move, rain
+	while (!q_cl.empty()) {
+		int ny = (q_cl.front().y + N + mv[d].y * s) % N;
+		int nx = (q_cl.front().x + N + mv[d].x * s) % N;
+		q_cl.pop();
+
+		Map[ny][nx] += 1;
+		check[ny][nx] = true;
+		q_cl_m.push({ ny, nx });
+	}
+
+	// duplicate
+	while (!q_cl_m.empty()) {
+		int y = q_cl_m.front().y;
+		int x = q_cl_m.front().x;
+		q_cl_m.pop();
+		for (int i = 0; i < 4; i++) {
+			int dy = y + dia[i].y;
+			int dx = x + dia[i].x;
+
+			if (dy >= 0 && dy < N && dx >= 0 && dx < N) {
+				if (Map[dy][dx]) Map[y][x] += 1;
+			}
+		}
+	}
+
+	// generate_cloud
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			if (Map[y][x] >= 2 && !check[y][x]) {
+				q_cl.push({ y, x });
+				Map[y][x] -= 2;
+			}
+		}
+	}
+
+}
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) cin >> Map[i][j];
+	}
+
+	int di, si;
+	for (int i = 0; i < M; i++) {
+		cin >> di >> si;
+		q.push({ di - 1, si % N});
+	}
+
+	q_cl.push({ N - 1, 0 });
+	q_cl.push({ N - 1, 1 });
+	q_cl.push({ N - 2, 0 });
+	q_cl.push({ N - 2, 1 });
+
+	info spe;
+	while (!q.empty()) {
+		spe = q.front();
+		q.pop();
+		di = spe.d; si = spe.s;
+		cloud_magic(N, di, si);
+	}
+
+	int ans = 0;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) ans += Map[y][x];
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
