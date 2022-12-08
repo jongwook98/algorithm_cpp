53616f3731716e7093d18779732963a37b998444
jongwook98 <oucsa1541@naver.com>
2022-12-08 23:35:18 +0900

    [Jongwook] Add softeer 문제해결
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

--- softeer/level3/비밀메뉴2.cpp
+++ softeer/level3/비밀메뉴2.cpp
@@ -2,6 +2,7 @@
 #include<cmath>
 
 // 백트래킹으로 해결
+// 정석적으로는 DP 로 해결 62번째 줄부터 DP 풀이
 
 using namespace std;
 
@@ -49,6 +50,41 @@ int main(int argc, char** argv)
 					cnt += 1;
 				}
 				ans = max(ans, cnt);
+			}
+		}
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
+
+#include<iostream>
+
+using namespace std;
+
+int arr[2][3000];
+int DP[3000][3000];
+
+int ans;
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M, K, T, U;
+	cin >> N >> M >> K;
+
+	for (int i = 0; i < N; i++) cin >> arr[0][i];
+	for (int i = 0; i < M; i++) cin >> arr[1][i];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (arr[0][i] == arr[1][j]) {
+				if (i == 0 || j == 0) DP[i][j] = 1;
+				else DP[i][j] = DP[i - 1][j - 1] + 1;
+				ans = max(ans, DP[i][j]);
 			}
 		}
 	}
---
+++ softeer/level4/트럭.cpp
@@ -0,0 +1,123 @@
+﻿#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+#define MAX 1000000000
+
+// 우선순위 큐, 사용자 정의 정렬
+
+using namespace std;
+
+typedef struct {
+	int s, p, m;
+}pro;
+
+typedef struct {
+	int ps, idx;
+}info;
+
+struct compare {
+	bool operator()(pro a, pro b) {
+		if (a.s != b.s) return a.s > b.s;
+		return a.p < b.p;
+	}
+};
+
+bool comp(info a, info b) {
+	return a.ps < b.ps;
+}
+
+int check[100000];
+int ans[100000];
+
+priority_queue<pro, vector<pro>, compare> pq;
+vector<info> v;
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	int pr, si, pi;
+	for (int i = 0; i < N; i++) {
+		cin >> pr;
+		for (int j = 0; j < pr; j++) {
+			cin >> si >> pi;
+			pq.push({ si, pi, i });
+		}
+	}
+
+	int M;
+	cin >> M;
+
+	int psi;
+
+	for (int i = 0; i < M; i++) {
+		cin >> psi;
+		v.push_back({ psi, i });
+	}
+
+	sort(v.begin(), v.end(), comp);
+
+	/*
+	// debug
+	pro ex;
+	while (!pq.empty()) {
+		ex = pq.top();
+		pq.pop();
+
+		cout << ex.s << " " << ex.p << " " << ex.m << '\n';
+	}
+	cout << "V" << '\n';
+
+	for (info inff : v) cout << inff.ps << " " << inff.idx << '\n';
+	*/
+
+	pro od;
+	info inf;
+
+	int cur = 0;
+	int cur_idx = 0;
+	long long sum_p = 0;
+
+	for (int i = 0; i < M; i++) ans[i] = -1;
+
+	while (!pq.empty() && cur_idx < v.size()) {
+		od = pq.top();
+		pq.pop();
+
+		cur = od.s;
+		if (check[od.m] == 0) {
+			check[od.m] = od.p;
+			sum_p += od.p;
+		}
+		else if (check[od.m] < od.p) {
+			sum_p -= check[od.m];
+			sum_p += od.p;
+			check[od.m] = od.p;
+		}
+
+		if (sum_p > MAX) sum_p = MAX;
+
+		while (cur_idx < v.size()) {
+			inf = v[cur_idx];
+			if (sum_p >= inf.ps) {
+				ans[inf.idx] = cur;
+				cur_idx += 1;
+			}
+			else break;
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		cout << ans[i] << " ";
+	}
+	cout << '\n';
+
+	return 0;
+}
