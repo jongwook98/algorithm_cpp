a630ac95f48a1922372c6e70512820e4a934d034
jongwook98 <oucsa1541@naver.com>
2022-12-23 17:02:07 +0900

    [Jongwook] Add DP 문제해결
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ DP/동전2.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int DP[10001];
+
+// 동전의 최소 개수를 구해야하므로
+// 기존의 DP 값과 동전의 가치를 뺀 DP 값 + 1 중에 작은 값으로 갱신
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 0; i <= k; i++) DP[i] = int(1e6);
+
+	int val;
+	while (n--) {
+		cin >> val;
+		pq.push(val);
+		if (val <= 10000) DP[val] = 1;
+	}
+
+	while (!pq.empty()) {
+		int iter = pq.top(); pq.pop();
+		for (int i = iter; i <= k; i++) {
+			DP[i] = min(DP[i], DP[i - iter] + 1);
+		}
+	}
+
+	//for (int i = 0; i <= k; i++) cout << DP[i] << " ";
+	if (DP[k] != int(1e6)) cout << DP[k] << '\n';
+	else cout << -1 << '\n';
+
+	return 0;
+}
---
+++ DP/파도반수열.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+
+using namespace std;
+
+long long DP[100];
+
+// int 범위 초과
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	DP[0] = 1;
+	DP[1] = 1;
+	DP[2] = 1;
+	DP[3] = 2;
+	DP[4] = 2;
+
+	for (int i = 5; i < 100; i++) DP[i] = DP[i - 1] + DP[i - 5];
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		int N;
+		cin >> N;
+		cout << DP[N - 1] << '\n';
+	}
+
+	return 0;
+}
