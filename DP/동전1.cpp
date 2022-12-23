f510439d32a16f2aad67ad6d8a8f927d9b79afbe
jongwook98 <oucsa1541@naver.com>
2022-12-23 16:14:51 +0900

    [Jongwook] Add DP 문제해결
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ DP/동전1.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int DP[10001];
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+// 순서를 고려하지 않고 사용한 동전의 구성만을 생각하므로
+// 작은 가치의 동전부터 점차 사용하는 것으로 생각
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, k;
+	cin >> n >> k;
+
+	int val;
+	while (n--) {
+		cin >> val;
+		pq.push(val);
+	}
+	
+	DP[0] = 1;
+	while (!pq.empty()) {
+		int iter = pq.top(); pq.pop();
+
+		for (int i = iter; i <= k; i++) {
+			DP[i] += DP[i - iter];
+		}
+	}
+
+	cout << DP[k] << '\n';
+
+	return 0;
+}
