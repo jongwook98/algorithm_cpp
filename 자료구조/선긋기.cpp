20f72fa63da0caa2771bd3d58483357b1b3fbbd7
jongwook98 <oucsa1541@naver.com>
2022-12-28 20:39:20 +0900

    [Jongwook] Add 자료구조 문제해결
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ 자료구조/선긋기.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+typedef struct {
+	int sp, ep;
+}info;
+
+struct compare {
+	bool operator()(info a, info b) {
+		if (a.sp != b.sp) return a.sp > b.sp;
+		return a.ep > b.ep;
+	}
+};
+
+priority_queue<info, vector<info>, compare> pq;
+
+int sp = -1000000001;
+int ep = -1000000001;
+
+int ans;
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	int csp, cep;
+	while (N--) {
+		cin >> csp >> cep;
+		pq.push({ csp, cep });
+	}
+
+	while (!pq.empty()) {
+		csp = pq.top().sp; cep = pq.top().ep;
+		pq.pop();
+
+		if (csp > ep) {
+			ans += ep - sp;
+			sp = csp;
+			ep = cep;
+		}
+		else {
+			ep = max(ep, cep);
+		}
+	}
+	ans += ep - sp;
+	cout << ans << '\n';
+
+	return 0;
+}
---
+++ 자료구조/흙길보수하기.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+typedef struct {
+	int sp, ep;
+}info;
+
+struct compare{
+	bool operator()(info a, info b) {
+		return a.sp > b.sp;
+	}
+};
+
+priority_queue<info, vector<info>, compare> pq;
+
+int cnt;
+int cur = -1;
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, L;
+	cin >> N >> L;
+
+	int csp, cep;
+	while (N--) {
+		cin >> csp >> cep;
+		pq.push({ csp, cep });
+	}
+
+	while (!pq.empty()) {
+		csp = pq.top().sp; cep = pq.top().ep;
+		pq.pop();
+
+		int ccnt = 0;
+
+		//cout << "cur : " << cur << " cnt : " << cnt << '\n';
+
+		if (cep - 1 > cur) {
+			if (cur >= csp) {
+				csp = cur + 1;
+			}
+			if (csp == cep) {
+				cnt += 1;
+				cur = csp + L - 1;
+			}
+			else {
+				ccnt = (cep - csp) / L;
+				if ((cep - csp) % L != 0) ccnt++;
+
+				cur = csp + L * ccnt - 1;
+			}
+		}
+		cnt += ccnt;
+
+		//cout << "cur : " << cur << " cnt : " << cnt << '\n';
+
+	}
+
+	cout << cnt << '\n';
+
+	return 0;
+}
