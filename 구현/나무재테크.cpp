f1feff5cf66aeaa3da74db8e191a6ee7c14366aa
jongwook98 <oucsa1541@naver.com>
2023-03-21 22:01:58 +0900

    [Jongwook] Add 구현 문제해결
    Signed-off-by: jongwook98 <oucsa1541@naver.com>

---
+++ 구현/나무재테크.cpp
@@ -0,0 +1,111 @@
+﻿#include<algorithm>
+#include<iostream>
+#include<vector>
+
+/*
+sort는 시간이 굉장히 오래 걸림
+문제 조건을 보고 sort를 굳이 쓸 필요 없다면 사용하지 말고
+sort 함수를 사용하지 않고 정렬할 수 있는 방법을 찾아볼 것
+*/
+
+using namespace std;
+
+int N;
+
+int Map[10][10];
+int addMap[10][10];
+
+int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+typedef struct {
+	int y, x;
+	int age;
+}info;
+
+vector<info> tree;
+vector<info> newtree;
+vector<info> dead_tree;
+
+bool compare(info a, info b) {
+	return a.age < b.age;
+}
+
+void spring() {
+	for (info t : tree) {
+		if (Map[t.y][t.x] < t.age) {
+			dead_tree.push_back(t);
+		}
+		else {
+			Map[t.y][t.x] -= t.age;
+			newtree.push_back({ t.y, t.x, t.age + 1 });
+		}
+	}
+
+	tree.clear();
+	for (info t : newtree) tree.push_back(t);
+	newtree.clear();
+}
+
+void summer() {
+	for (info t : dead_tree) {
+		Map[t.y][t.x] += int(t.age / 2);
+	}
+	dead_tree.clear();
+}
+
+void fall() {
+	for (info t : tree) {
+		if (t.age % 5 == 0) {
+			for (int i = 0; i < 8; i++) {
+				int ny = t.y + dy[i]; int nx = t.x + dx[i];
+
+				if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
+					newtree.push_back({ ny, nx, 1 });
+				}
+			}
+		}
+	}
+	for (info t : tree) newtree.push_back(t);
+	tree.clear();
+	
+	for (info t : newtree) tree.push_back(t);
+	newtree.clear();
+}
+
+void winter() {
+	for (int y = 0; y < N; y++) for (int x = 0; x < N; x++) Map[y][x] += addMap[y][x];
+}
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int M, K;
+	cin >> N >> M >> K;
+
+	for (int x = 0; x < N; x++) for (int y = 0; y < N; y++) {
+		Map[y][x] = 5;
+		cin >> addMap[y][x];
+	}
+
+	int x, y, z;
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y >> z;
+		tree.push_back({ y - 1, x - 1, z });
+	}
+
+	sort(tree.begin(), tree.end(), compare);
+	while (K--) {
+		spring();
+		summer();
+		fall();
+		winter();
+	}
+
+	cout << tree.size() << '\n';
+
+	return 0;
+}
---
+++ 기타/순열,조합.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+
+vector<int> a;
+
+bool check[5];
+int arr[5];
+
+int N = 3;
+
+void permutation(int r, int cur) {
+	if (r == 3) {
+		for (int iter : a) cout << iter << " ";
+		cout << '\n';
+	}
+
+	for (int i = cur; i < 5; i++) {
+		if (check[i]) continue;
+
+		check[i] = true;
+		a.push_back(arr[i]);
+		permutation(r + 1, cur + 1);
+		a.pop_back();
+		check[i] = false;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	memset(check, false, sizeof(check));
+
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	arr[3] = 4;
+	arr[4] = 5;
+
+	permutation(0, 0);
+}
